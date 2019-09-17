67c795daf345bf9a64d23d1f3cccccca466b47f7
Nicholas Upshaw <nicholas.upshaw@yahoo.com>
2019-09-17 18:36:23 -0400

    Add images and pieces of the javascript

--- javascript/game.js
+++ javascript/game.js
@@ -1,12 +1,21 @@
-var Player = 100;
-var Enemy = 100;
+
+var player = 100;
+var enemy = 100;
+var playerHit = Math.floor(Math.random() * 10);
+var enemyCounter = Math.random() * 7;
+var playerCharcter = ("");
+var bonus = 25;
 
 
+
+$(document).ready(function() {
 // Player selection
+$(".Charcter").on("$click" )
 
 // Enemy selection
+$(".charcter").on("$click")
 
-
+ $(".attack").on("$click")
 // Player Health bar
 
 // Enemy Health bar
@@ -14,3 +23,5 @@ var Enemy = 100;
 // Player Attack
 
 // Enemy Counter Attack
+
+});
