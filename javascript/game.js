3a5226b1be5be6baac5b89c0e6721669f2a171e8
Nicholas Upshaw <nicholas.upshaw@yahoo.com>
2019-09-17 21:35:54 -0400

    Ordering javascript to work prperly

--- javascript/game.js
+++ javascript/game.js
@@ -2,41 +2,48 @@
 var player = 100;
 var enemy = 100;
 var playerHit = Math.floor(Math.random() * 10);
-var enemyCounter = Math.random() * 8;
-var playerCharcter = ("");
+var enemyCounter = Math.random() * 7;
+var playerCharacter = ("");
 var bonus = 50;
 
 
-
-$(window).onload(function() {
-
+window.onload = function() {
+    $(".character").on("$click", playerSelect);
+    $(".character").on("$click", opponentSelect);
+    $(".attack").on("$click", attack);
+};
     // Player Health bar
-$("#player").text("100")
+$("#player").text(player)
    // Enemy Health bar
-$("#opponent").text("100")
+$("#opponent").text(enemy)
 
 // Player selection
-$(".charcter").on("$click", playerSelect);
+
 function playerSelect() {
     
 }
 
 // Enemy selection
-$(".charcter").on("$click", opponentSelect );
+;
 function opponentSelect() {
     
 }
 
 // Player Attack
-$(".attack").on("$click", attack)
+
 function attack() {
     
-    counterAttack();
+        
+    counterAttack()
 }
 
 // Enemy Counter Attack
 function counterAttack() {
     
 }
+    
+    
+    
 
-});
+
+//round end add player health bonus for win
