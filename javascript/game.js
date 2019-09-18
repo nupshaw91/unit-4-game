f3e5ba16b02a561a7554f2e15c476e9cb4b592f0
Nicholas Upshaw <nicholas.upshaw@yahoo.com>
2019-09-17 20:08:25 -0400

    Adding function skeleton, bootstrap and css styling

--- javascript/game.js
+++ javascript/game.js
@@ -2,26 +2,41 @@
 var player = 100;
 var enemy = 100;
 var playerHit = Math.floor(Math.random() * 10);
-var enemyCounter = Math.random() * 7;
+var enemyCounter = Math.random() * 8;
 var playerCharcter = ("");
-var bonus = 25;
+var bonus = 50;
 
 
 
-$(document).ready(function() {
+$(window).onload(function() {
+
+    // Player Health bar
+$("#player").text("100")
+   // Enemy Health bar
+$("#opponent").text("100")
+
 // Player selection
-$(".Charcter").on("$click" )
+$(".charcter").on("$click", playerSelect);
+function playerSelect() {
+    
+}
 
 // Enemy selection
-$(".charcter").on("$click")
-
- $(".attack").on("$click")
-// Player Health bar
-
-// Enemy Health bar
+$(".charcter").on("$click", opponentSelect );
+function opponentSelect() {
+    
+}
 
 // Player Attack
+$(".attack").on("$click", attack)
+function attack() {
+    
+    counterAttack();
+}
 
 // Enemy Counter Attack
+function counterAttack() {
+    
+}
 
 });
