6d2d119c0ec027a0dfc4d521f68d4c570f45363f
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2019-10-01 19:52:14 -0400

    appending players to stage

--- javascript/game.js
+++ javascript/game.js
@@ -5,71 +5,84 @@ var playerHit = Math.floor(Math.random() * 12);
 var enemyCounter = Math.floor(Math.random() * 6);
 var playerCharacter = ("");
 var bonus = 50;
+var cast = ["Yoda", "quiGon", "obiWan", "emperorPalatine", "darthVader", "kyloRen"];
 
 
-window.onload = function() {
-    $("#yoda").click(function(){
-        $("<img src/>")
-      console.log("yoda was clicked");
+
+window.onload = function () {
+    $("#yoda").click(function () {
+        for (let i = 0; i < cast.length; i++) {
+        }
+        if (".player" === "#" + cast[i].length){
+        $("#yoda").appendTo(".opponent")}
+
+        else $("#yoda").appendTo(".player");
+
+        //   console.log(cast);
     });
 
-    $("#quiGon").click(function(){
+    $("#quiGon").click(function () {
+        for (let i = 0; i < cast.length; i++) {
+        }
+        if (".player" === "#" + cast[i].length){
+        $("#quiGon").appendTo(".opponent")}
+
+        else $("#quiGon").appendTo(".player");
 
         console.log("quigon was clicked");
     });
-    $("#obiWan").click(function(){
+    $("#obiWan").click(function () {
         console.log("obi was clicked");
     });
 
-    $("#emperorPalpatine").click(function(){
+    $("#emperorPalpatine").click(function () {
         console.log("palpatine was clicked");
     });
-    $("#darthVader").click(function(){
+    $("#darthVader").click(function () {
         console.log("vader was clicked");
     });
 
-    $("#kyloRen").click(function(){
+    $("#kyloRen").click(function () {
         console.log("kylo was clicked");
     });
 
-    $(".attack").click(function() {
+    $(".attack").click(function () {
         attack()
         $("#player").text(player)
         counterAttack()
         $("#opponent").text(enemy)
-        
+
     });
 };
 
-    // Player Health bar
+// Player Health bar
 $("#player").text(player)
-   // Enemy Health bar
+// Enemy Health bar
 $("#opponent").text(enemy)
 
 // Player selection
 
 function playerSelect() {
-    
+
 }
 
 // Enemy selection
 ;
 function opponentSelect() {
-    
+
 }
 
 // Player Attack
 
 function attack() {
-   return enemy-=playerHit;
+    return enemy -= playerHit;
 }
 
 // Enemy Counter Attack
 function counterAttack() {
-    return player-=enemyCounter;
+    return player -= enemyCounter;
 }
- //round end add player health bonus for win   
-    
+ //round end add player health bonus for win
 
-    
-    
+
+
