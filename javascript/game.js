bbe7afd54501994d4f7fe57218c54075d0ea772c
Nicholas Upshaw <nicholas.upsha@yahoo.com>
2019-10-05 12:20:58 -0400

    player select working 65% done

--- javascript/game.js
+++ javascript/game.js
@@ -1,88 +1,127 @@
 
 var player = 100;
 var enemy = 100;
-var playerHit = Math.floor(Math.random() * 12);
-var enemyCounter = Math.floor(Math.random() * 6);
-var playerCharacter = ("");
-var bonus = 50;
-var cast = ["Yoda", "quiGon", "obiWan", "emperorPalatine", "darthVader", "kyloRen"];
+var game = [
+    cast = ["yoda", "quiGon", "obiWan", "emperorPalpatine", "darthVader", "kyloRen"],
+    health = 100,
+    playerHit = Math.floor(Math.random() * 12),
+    enemyCounter = Math.floor(Math.random() * 6),
+    bonus = 50,
+    player = 100,
+    enemy = 100,
 
-
+]
 
 window.onload = function () {
-    $("#yoda").click(function () {
-        for (let i = 0; i < cast.length; i++) {
+
+    for (let i = 0; i < cast.length; i++) {
+        var player = $("#" + cast[i]);
+
+        player.addClass("player");
+        player.attr("data-cast", cast[i]);
+
+        function playerOne() {
+            $("#" + cast[i]).appendTo("#player")
+            cast.length--;
+            console.log("You did it");
+        };
+        function playerTwo() {
+            console.log("why didn't i append");
+            $("#" + cast[i]).appendTo("#opponent");
+        };
+        function combatants() {
+            if (cast.length >= 6) {
+                playerOne()
+            }
+            else {
+                playerTwo()
+            }
         }
-        if (".player" === "#" + cast[i].length){
-        $("#yoda").appendTo(".opponent")}
+        $(player).click(combatants)
 
-        else $("#yoda").appendTo(".player");
 
-        //   console.log(cast);
-    });
 
-    $("#quiGon").click(function () {
-        for (let i = 0; i < cast.length; i++) {
-        }
-        if (".player" === "#" + cast[i].length){
-        $("#quiGon").appendTo(".opponent")}
 
-        else $("#quiGon").appendTo(".player");
 
-        console.log("quigon was clicked");
-    });
-    $("#obiWan").click(function () {
-        console.log("obi was clicked");
-    });
+    };
 
-    $("#emperorPalpatine").click(function () {
-        console.log("palpatine was clicked");
-    });
-    $("#darthVader").click(function () {
-        console.log("vader was clicked");
-    });
 
-    $("#kyloRen").click(function () {
-        console.log("kylo was clicked");
-    });
 
-    $(".attack").click(function () {
-        attack()
-        $("#player").text(player)
-        counterAttack()
-        $("#opponent").text(enemy)
 
-    });
+    // console.log(cast)
+    //     $("#yoda").click(function () {
+    //         for (let i = 0; i < cast.length; i++) {
+
+    //         if (".player" == "#" + cast[i]){
+
+    //         $("#yoda").appendTo(".opponent")}
+
+    //         else $("#yoda").appendTo(".player");
+    //         }
+    //           console.log(cast[1]);
+    //     });
+
+    //     $("#quiGon").click(function () {
+
+    //         for (let i = 0; i < cast.length; i++) {
+    //         if (".player" ===   cast[i]){     
+    //         $("#quiGon").appendTo(".opponent")}
+    //         else $("#quiGon").appendTo(".player");
+    //         }
+    //         console.log("quigon was clicked");
+    //     // });
+    //     // $("#obiWan").click(function () {
+    //     //     console.log("obi was clicked");
+    //     // });
+
+    //     // $("#emperorPalpatine").click(function () {
+    //     //     console.log("palpatine was clicked");
+    //     // });
+    //     // $("#darthVader").click(function () {
+    //     //     console.log("vader was clicked");
+    //     // });
+
+    //     // $("#kyloRen").click(function () {
+    //     //     console.log("kylo was clicked");
+    //     // });
+
+    //     $(".attack").click(function () {
+    //         attack()
+    //         $("#player").text(player)
+    //         counterAttack()
+    //         $("#opponent").text(enemy)
+
+    //     });
+    // };
+
+    // Player Health bar
+    $("#player").text(game.player)
+    // Enemy Health bar
+    $("#opponent").text(game.health)
+
+    // Player selection
+
+    function playerSelect() {
+
+    }
+
+    // Enemy selection
+    ;
+    function opponentSelect() {
+
+    }
+
+    // Player Attack
+
+    function attack() {
+        return enemy -= playerHit;
+    }
+
+    // Enemy Counter Attack
+    function counterAttack() {
+        return player -= enemyCounter;
+    }
+    //round end add player health bonus for win
+
+
 };
-
-// Player Health bar
-$("#player").text(player)
-// Enemy Health bar
-$("#opponent").text(enemy)
-
-// Player selection
-
-function playerSelect() {
-
-}
-
-// Enemy selection
-;
-function opponentSelect() {
-
-}
-
-// Player Attack
-
-function attack() {
-    return enemy -= playerHit;
-}
-
-// Enemy Counter Attack
-function counterAttack() {
-    return player -= enemyCounter;
-}
- //round end add player health bonus for win
-
-
-
